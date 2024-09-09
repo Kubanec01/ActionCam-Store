b016278fb6699bded914be1fc26099021cb48dbe
Jakub Roman <jakub.m.roman@gmail.com>
2024-09-09 22:40:28 +0200

    dokoncenie responsivity pre ShopPage, ShopNavbar treba dokoncit

--- src/pages/shopPage/home/camerasStore/components/accessoryComplement/AccessoryComplement.tsx
+++ src/pages/shopPage/home/camerasStore/components/accessoryComplement/AccessoryComplement.tsx
@@ -3,9 +3,12 @@ import { useShoppingCart } from "../../../../../../context/ShoppingCartContext";
 import { productsList } from "../../../../../../data/productsList";
 import style from "./accessoryComplement.module.css";
 import { Trans, useTranslation } from "react-i18next";
+import { useScreenSize } from "../../../../../../hooks/useScreenSize";
 
 export function AccessoryComplement() {
   const { t } = useTranslation();
+
+  const windowWidth = useScreenSize();
 
   const { increaseProductsCount } = useShoppingCart();
 
@@ -25,36 +28,70 @@ export function AccessoryComplement() {
             className="rounded-2xl mx-auto max-w-[1300px] bg-[#050505] p-6 shadow-inner"
             key={p.id}
           >
-            <div className="flex lg:flex-row flex-col">
-              {/* LEFT */}
-              <div className="w-[50%]">
-                <h1 className="text-[#ffffffd5] text-6xl mt-[6.25rem]">
-                  {p.name}
-                </h1>
-                <p className="text-[#ffffff8e] text-xl w-[84%] mt-[3.125rem]">
-                  {p.desc}
-                </p>
-                <div className="flex w-[300px] justify-between my-[3.125rem]">
-                  <p
-                    className={`${style.colorText} text-4xl`}
-                  >{`$${p.price}`}</p>
-                  <button
-                    className={`${style.button} text-white text-2xl`}
-                    onClick={() => increaseProductsCount(p.id)}
-                  >
-                    {t("shop.itemButton")}
-                  </button>
+            {windowWidth > 1026 ? (
+              <div className="flex lg:flex-row flex-col">
+                {/* LEFT */}
+                <div className="w-[50%]">
+                  <h1 className="text-[#ffffffd5] text-6xl mt-[6.25rem]">
+                    {p.name}
+                  </h1>
+                  <p className="text-[#ffffff8e] text-xl w-[84%] mt-[3.125rem]">
+                    {p.desc}
+                  </p>
+                  <div className="flex w-[300px] justify-between my-[3.125rem]">
+                    <p
+                      className={`${style.colorText} text-4xl`}
+                    >{`$${p.price}`}</p>
+                    <button
+                      className={`${style.button} text-white text-2xl`}
+                      onClick={() => increaseProductsCount(p.id)}
+                    >
+                      {t("shop.itemButton")}
+                    </button>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-[50%] mt-[7.5rem]">
+                  <img
+                    className="object-cover w-full h-full"
+                    src={p.image}
+                    alt={`${p.name}-img`}
+                  />
                 </div>
               </div>
-              {/* RIGHT */}
-              <div className="w-[50%] mt-[7.5rem]">
-                <img
-                  className="object-cover w-full h-full"
-                  src={p.image}
-                  alt={`${p.name}-img`}
-                />
-              </div>
-            </div>
+            ) : (
+                <div>
+                  {/* TITLE AND DESC */}
+                  <div className="flex flex-col  text-center items-center">
+                    <h1 className="text-[#ffffffd5] md:text-5xl text-4xl mt-[6.25rem]">
+                      {p.name}
+                    </h1>
+                    <p className="text-[#ffffff8e] md:text-lg text-base sm:w-[80%] md:mt-8 mt-4">
+                      {p.desc}
+                    </p>
+                  </div>
+                  {/* IMAGE */}
+                  <div className="mx-auto md:w-[60%] sm:w-[70%] w-[80%]">
+                    <img
+                      className="object-cover w-full h-full"
+                      src={p.image}
+                      alt={`${p.name}-img`}
+                    />
+                  </div>
+                  {/* PRICE AND BUTTON */}
+                  <div className="flex md:w-[55%] sm:w-[70%] w-[84%] mx-auto justify-between my-[3.125rem]">
+                    <p
+                      className={`${style.colorText} md:text-4xl text-3xl`}
+                    >{`$${p.price}`}</p>
+                    <button
+                      className={`${style.button} text-white md:text-2xl text-2xl`}
+                      onClick={() => increaseProductsCount(p.id)}
+                    >
+                      {t("shop.itemButton")}
+                    </button>
+                  </div>
+                </div>
+            )}
           </div>
         );
       }
