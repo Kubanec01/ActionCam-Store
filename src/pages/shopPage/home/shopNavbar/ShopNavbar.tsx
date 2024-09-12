814288ef4240abf5e9eff59e7dcfb3da7914bb6f
Jakub Roman <jakub.m.roman@gmail.com>
2024-09-12 22:26:25 +0200

    new commit

--- src/pages/shopPage/home/shopNavbar/ShopNavbar.tsx
+++ src/pages/shopPage/home/shopNavbar/ShopNavbar.tsx
@@ -10,19 +10,21 @@ import { PiShoppingCartLight } from "react-icons/pi";
 import { IoReturnUpBackOutline } from "react-icons/io5";
 import { useState } from "react";
 import { CartItem } from "./CartItem";
-import emptyCartImg from "../../../../assets/empty-cart-img.png"
+import emptyCartImg from "../../../../assets/empty-cart-img.png";
 import { useTranslation } from "react-i18next";
 import { useShoppingCart } from "../../../../context/ShoppingCartContext";
-import { productsList } from "../../../../data/productsList"
+import { productsList } from "../../../../data/productsList";
+import { useScreenSize } from "../../../../hooks/useScreenSize";
 
 export function ShopNavbar() {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
 
   // STYLES
   const link = `${style.link}`;
   const linkIcon = "mx-auto text-3xl";
 
   const { cartProductsCount, cartProducts } = useShoppingCart();
+  const screenWindow = useScreenSize();
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -32,86 +34,157 @@ export function ShopNavbar() {
     const product = productsList.find((p) => p.id === currProduct.id);
     return total + (product?.price || 0) * currProduct.count;
   }, 0);
-  const totalPrice = Math.round((priceCalc * 100)) / 100;
+  const totalPrice = Math.round(priceCalc * 100) / 100;
 
   return (
     <Navbar className="fixed z-[1100] w-full px-2">
-      <Container
-        style={{
-          borderRadius: "10px 10px 20px 20px",
-          backdropFilter: "blur(10px)",
-        }}
-        className={`${isOpen ? style.openBar : style.closedBar}
+      {screenWindow > 870 ? (
+        <Container
+          style={{
+            borderRadius: "10px 10px 20px 20px",
+            backdropFilter: "blur(10px)",
+          }}
+          className={`${isOpen ? style.openBar : style.closedBar}
          max-w-[1000px] mx-auto flex flex-col justify-between mt-[1.25rem] bg-[#1a19198f]
         border-2 border-[#f1f1f193] overflow-hidden`}
-      >
-        <Nav className="mx-auto w-[90%] h-[5.625rem] flex items-center justify-center text-[#ffffffac] text-lg gap-32">
-          <a className={`${link} text-center`} href="#">
-            <IoCameraOutline className={linkIcon} />
-            {t("navbar.cameras")}
-          </a>
-          <a className={link} href="#">
-            <AiOutlineStar className={linkIcon} />
-            {t("navbar.accessories")}
-          </a>
-          <Link className={link} to="/about">
-            <AiOutlineQuestion className={linkIcon} />
-            {t("navbar.about")}
-          </Link>
-          <Link className={link} to="/">
-            <HiOutlineHome className={linkIcon} />
-            {t("navbar.home")}
-          </Link>
-          <div className="relative">
-            <Button
-              onClick={() => setIsOpen(isOpen => !isOpen)}
-              className={`${style.shoppingCartIcon}`}
-            >
-              {isOpen ? (
-                <IoReturnUpBackOutline className="mx-auto text-[2.8rem]" />
-              ) : (
-                <PiShoppingCartLight className="mx-auto text-[2.8rem]" />
-              )}
-            </Button>
-            {productsCount > 0 ? (
-              <span
-                className="absolute bg-[#5b29d0] text-white border-2 border-[#1a1919] rounded-[100%] w-[1.625rem] aspect-square 
+        >
+          <Nav className="mx-auto w-[90%] h-[5.625rem] flex items-center justify-center text-[#ffffffac] text-lg gap-32">
+            <a className={`${link} text-center`} href="#">
+              <IoCameraOutline className={linkIcon} />
+              {t("navbar.cameras")}
+            </a>
+            <a className={link} href="#">
+              <AiOutlineStar className={linkIcon} />
+              {t("navbar.accessories")}
+            </a>
+            <Link className={link} to="/about">
+              <AiOutlineQuestion className={linkIcon} />
+              {t("navbar.about")}
+            </Link>
+            <Link className={link} to="/">
+              <HiOutlineHome className={linkIcon} />
+              {t("navbar.home")}
+            </Link>
+            <div className="relative">
+              <Button
+                onClick={() => setIsOpen((isOpen) => !isOpen)}
+                className={`${style.shoppingCartIcon}`}
+              >
+                {isOpen ? (
+                  <IoReturnUpBackOutline className="mx-auto text-[2.8rem]" />
+                ) : (
+                  <PiShoppingCartLight className="mx-auto text-[2.8rem]" />
+                )}
+              </Button>
+              {productsCount > 0 ? (
+                <span
+                  className="absolute bg-[#5b29d0] text-white border-2 border-[#1a1919] rounded-[100%] w-[1.625rem] aspect-square 
            text-sm top-0 -right-2 flex justify-center items-center font-medium"
-              >
-                {productsCount < 100 ? productsCount : "99+"}
-              </span>
-            ) : (
-              ""
-            )}
-          </div>
-        </Nav>
-        {isOpen ? (
-          <div className="w-full h-[84%]">
-            <div className="h-[88%] flex justify-normal mx-4 ">
-              <ul
-                className={`${style.shoppingCartList} w-full h-full overflow-y-scroll pr-2`}
-              >
-                {productsCount > 0 ? 
-                cartProducts.map((p) => {
-                  return <CartItem key={p.id} {...p} />;
-                })
-                :
-                <div className="w-[60%] h-[80%] mx-auto">
-                <img
-                className="opacity-[0.09] w-full h-full object-cover"
-                src={emptyCartImg} alt="empty-cart-img" />
+                >
+                  {productsCount < 100 ? productsCount : "99+"}
+                </span>
+              ) : (
+                ""
+              )}
+            </div>
+          </Nav>
+          {isOpen ? (
+            <div className="w-full h-[84%]">
+              <div className="h-[88%] flex justify-normal mx-4 ">
+                <ul
+                  className={`${style.shoppingCartList} w-full h-full overflow-y-scroll pr-2`}
+                >
+                  {productsCount > 0 ? (
+                    cartProducts.map((p) => {
+                      return <CartItem key={p.id} {...p} />;
+                    })
+                  ) : (
+                    <div className="w-[60%] h-[80%] mx-auto">
+                      <img
+                        className="opacity-[0.09] w-full h-full object-cover"
+                        src={emptyCartImg}
+                        alt="empty-cart-img"
+                      />
+                    </div>
+                  )}
+                </ul>
               </div>
-              }
-              </ul>
+              <div className="h-full flex justify-end">
+                <p className="text-white text-3xl mr-6 mt-5">{`$${totalPrice}`}</p>
+              </div>
             </div>
-            <div className="h-full flex justify-end">
-              <p className="text-white text-3xl mr-6 mt-5">{`$${totalPrice}`}</p>
+          ) : (
+            ""
+          )}
+        </Container>
+      ) : (
+        <Container
+          style={{
+            borderRadius: "10px 10px 20px 20px",
+            backdropFilter: "blur(10px)",
+          }}
+          className={`${isOpen ? style.mdOpenBar : style.mdClosedBar}
+       w-[90%] mx-auto flex flex-col justify-between mt-[1.25rem] bg-[#1a19198f]
+      border-2 border-[#f1f1f193] overflow-hidden`}
+        >
+          <Nav className="mx-auto w-[90%] h-[5.625rem] flex items-center justify-between text-[#ffffffac] text-lg">
+            <div className="flex gap-20 w-[50%]">
+              <Link to="/about">
+                <AiOutlineQuestion className="sm:text-[2.7rem] text-[2.5rem]" />
+              </Link>
+              <Link to="/">
+                <HiOutlineHome className="sm:text-[2.7rem] text-[2.5rem]" />
+              </Link>
             </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </Container>
+            <div className="relative">
+              <Button
+                onClick={() => setIsOpen((isOpen) => !isOpen)}
+                className={`${style.shoppingCartIcon}`}
+              >
+                {isOpen ? (
+                  <IoReturnUpBackOutline className="mx-auto sm:text-[2.8rem]" />
+                ) : (
+                  <PiShoppingCartLight className="mx-auto text-[2.8rem]" />
+                )}
+              </Button>
+              {productsCount > 0 ? (
+                <span
+                  className="absolute bg-[#5b29d0] text-white border-2 border-[#1a1919] rounded-[100%] w-[1.625rem] aspect-square 
+           text-sm top-0 -right-2 flex justify-center items-center font-medium"
+                >
+                  {productsCount < 100 ? productsCount : "99+"}
+                </span>
+              ) : (
+                ""
+              )}
+            </div>
+          </Nav>
+          {isOpen ? (
+            <div className="w-full h-[84%]">
+              <div className="h-[88%] flex justify-normal mx-4 ">
+                <ul
+                  className={`${style.shoppingCartList} w-full h-full overflow-y-scroll pr-2`}
+                >
+                  {productsCount > 0 ? (
+                    cartProducts.map((p) => {
+                      return <CartItem key={p.id} {...p} />;
+                    })
+                  ) : (
+                    <div className="border w-full h-full">
+
+                    </div>
+                  )}
+                </ul>
+              </div>
+              <div className="h-full flex justify-end">
+                <p className="text-white text-3xl mr-6 mt-5">{`$${totalPrice}`}</p>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </Container>
+      )}
     </Navbar>
   );
 }
